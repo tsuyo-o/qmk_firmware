5f9808fbc923737688fde20f25e9b9a1d9f0d339
me <me@localhost>
2021-08-30 19:09:39 +0900

    add sft_t

--- keyboards/ergo42mk2/keymaps/default/keymap.c
+++ keyboards/ergo42mk2/keymaps/default/keymap.c
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, KC_Q,        KC_W,    KC_E,           KC_R,          KC_T,                                                      KC_Y,          KC_U,   KC_I,    KC_O,    KC_P,              XXXXXXX, \
     XXXXXXX, KC_A,        KC_S,    LT(SYMB2,KC_D), KC_F,          KC_G,  XXXXXXX,                           XXXXXXX,         KC_H,          KC_J,   KC_K,    KC_L,    KC_COLN,           XXXXXXX, \
     XXXXXXX, SFT_T(KC_Z), KC_X,    KC_C,           KC_V,          KC_B,                                                      KC_N,          KC_M,   KC_COMM, KC_DOT,  SFT_T(KC_SLSH),    XXXXXXX, \
-    XXXXXXX, XXXXXXX,     XXXXXXX, KC_TAB,         OSM(MOD_LCTL), LT(SYMB,KC_SPC), XXXXXXX, XXXXXXX, LT(SYMB,KC_SPC), OSM(MOD_RCTL), KC_ESC,          XXXXXXX, XXXXXXX,          XXXXXXX  \
+    XXXXXXX, XXXXXXX,     XXXXXXX, SFT_T(KC_TAB),  OSM(MOD_LCTL), LT(SYMB,KC_SPC), XXXXXXX,        XXXXXXX, LT(SYMB,KC_SPC), OSM(MOD_RCTL), SFT_T(KC_ESC),   XXXXXXX, XXXXXXX,          XXXXXXX \
   ),
 
   /* SYMB
