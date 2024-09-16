09cc2ebacb910442f485e294feedd2253bcfe275
me <me@matrix.local>
2024-09-16 22:22:06 +0900

    update warning comment for USB_WAIT_FOR_ENUMERATION

--- keyboards/bastardkb/charybdis/3x5/keymaps/mac/rules.mk
+++ keyboards/bastardkb/charybdis/3x5/keymaps/mac/rules.mk
@@ -1,4 +1,5 @@
 MOUSEKEY_ENABLE = yes
-// only for keyboard quantizer
-// without this, I had to unplug & plug usb cable every time.
+# only for keyboard quantizer
+#   without this, I had to unplug & plug usb cable every time.
+# WARN: Do NOT use this with 'EE_HANDS'! Otherwise the slave side won't work
 USB_WAIT_FOR_ENUMERATION = yes
