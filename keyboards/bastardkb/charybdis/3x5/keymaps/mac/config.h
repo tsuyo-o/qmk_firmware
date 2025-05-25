da2ff4023dfcea477de306d3296ad7957d0d434d
me <me@matrix.local>
2025-05-25 21:34:51 +0900

    add ee_hands, remove a config for quantizer, extend tapping term

--- keyboards/bastardkb/charybdis/3x5/keymaps/mac/config.h
+++ keyboards/bastardkb/charybdis/3x5/keymaps/mac/config.h
@@ -12,11 +12,13 @@
 #define AUTO_MOUSE_TIME	650
 
 // https://github.com/qmk/qmk_firmware/blob/master/docs/getting_started_introduction.md
-//#undef MASTER_RIGHT
-//#define MASTER_LEFT
-//#define EE_HANDS
+#undef MASTER_RIGHT
+#define MASTER_LEFT
+
+// https://github.com/qmk/qmk_firmware/blob/3ac9259742bfd428f71c31dbf2bfedace2a7f91b/keyboards/lets_split/readme.md#setting-ee_hands-to-use-either-hands-as-master
+#define EE_HANDS
 
 // default is 200
-#define TAPPING_TERM 300
+#define TAPPING_TERM 400
 // keep Hold state if the time of next key-typing < TAPPING_TERM.
 #define PERMISSIVE_HOLD
--- keyboards/bastardkb/charybdis/3x5/keymaps/mac/rules.mk
+++ keyboards/bastardkb/charybdis/3x5/keymaps/mac/rules.mk
@@ -2,4 +2,4 @@ MOUSEKEY_ENABLE = yes
 # only for keyboard quantizer
 #   without this, I had to unplug & plug usb cable every time.
 # WARN: It won't work as "slave" halves, so need to use as "master" always.
-USB_WAIT_FOR_ENUMERATION = yes
+# USB_WAIT_FOR_ENUMERATION = yes
